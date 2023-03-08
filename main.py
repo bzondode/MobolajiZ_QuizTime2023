fb5125ea220719eda078e90dae832fc395621eab
bzondode <100369548+bzondode@users.noreply.github.com>
2023-03-08 15:36:18 +0000

    Used a FOR loop to repeat process. This way, I avoided repeating code.

--- main.py
+++ main.py
@@ -25,91 +25,27 @@ note = 0
 questions = ("Q1. There are seven continents in the world.", "Q2. Insects have six legs.", "Q3. The Earth is the closest planet to the Sun.", "Q4. Russia is the largest country by area in the world.", "Q5. Ancient Eqyptian civilization was responsible for the pyramids.")
 answers = ("T", "T", "F", "T", "T")
 
-# Display question
-print(questions[0])
+numOfQuestions = len(questions)
+for num in range(numOfQuestions):
 
-# Check user entered only "T" or "F"
-answer = ""
+  # Display next question
+  print(questions[num])
 
-while(answer != "T" and answer != "F"):
+  # Check user entered only "T" or "F"
+  answer = ""
+
+  while(answer != "T" and answer != "F"):
   
-  # Prompt user for input
-  answer = input("  Please enter T for True or F for False: ")
+    # Prompt user for input
+    answer = input("  Please enter T for True or F for False: ")
 
-# Check user entered correctly and give 1 point 
-if(answer == answers[0]):
-  exact += 1
+  # Give 1 point if user made the correct entry 
+  if(answer == answers[num]):
+    exact += 1
 
-# Proceed to the next question
-  note += 1
-print()
-
-print(questions[1])
-
-# Check user entered only "T" or "F"
-answer = ""
-
-while(answer != "T" and answer != "F"):
-  # Prompt user for input
-  answer = input("  Please enter T for True or F for False: ")
-
-# Give 1 point if user made the correct entry
-if(answer == answers[1]):
-  exact += 1
-
-  # Proceed to the next question
-  note += 1
-print()
-  
-# Display next question
-print(questions[2])
-
-# Check user entered only "T" or "F"
-answer = ""
-
-while(answer != "T" and answer != "F"):
-  # Prompt user for input
-  answer = input("  Please enter T for True or F for False: ")
-  
-# Give 1 point if user made the correct entry 
-if(answer == answers[2]):
-  exact += 1
-
-  # Proceed to the next question
-  note += 1
-print()
-
-# Display next question
-print(questions[3])
-  
-# Check user entered only "T" or "F"
-answer = ""
-
-while(answer != "T" and answer != "F"):
-  # Prompt user for input
-  answer = input("  Please enter T for True or F for False: ")
-  
-# Give 1 point if user made the correct entry 
-if(answer == answers[3]):
-  exact += 1
-
-  # Proceed to the next questions
-  note += 1
-print()
-
-# Display next question
-print(questions[4])
-  
-answer = ""
-
-while(answer != "T" and answer != "F"):
-  # Re-prompt user to enter T or F
-  answer = input("  Please enter T for True or F for False: ")
-  
-if(answer == answers[4]):
-  exact += 1
-
-print()
+  # Proceed to the next question(s)
+    note += 1
+  print()
 
 # Display to user # of questions they got right
 print(f"You got {exact} out of {len(questions)} correct! Thanks for playing!")
